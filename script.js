eb023bf217192ae9dfe61f0aa04bba427cef976a
karthikkuddigana <63944339+Karthikkuddigana@users.noreply.github.com>
2024-10-24 22:48:16 +0530

    Added the functionality to delete projects and tasks

--- script.js
+++ script.js
@@ -9,24 +9,28 @@ const inputDesc=document.querySelector("#ProjectDesc");
 const sideHeader=document.querySelector("#SideHeader"); 
 const taskContent=document.querySelector("#TaskContent"); 
 const inputTask=document.querySelector("#TaskName"); 
+const projectDisplay=document.querySelector("#ProjectDisplay"); 
 let currentProject; 
+//AddTask dialog Button
 taskContent.addEventListener("click",(e)=>{
     // console.log("div clicked"); 
     if(e.target && e.target.matches("button#AddTask")){
         dialogTask.showModal(); 
     }
 })
+//AddProject dialog button 
 showDialogBtn.addEventListener("click",(e)=>{
     document.getElementById("myform").reset();
     dialogProject.showModal(); 
 })
+//AddProject logic 
 submitDialogBtn.addEventListener('click',(e)=>{
     // e.preventDefault(); 
     let temp= new project(inputName.value,inputDesc.value)
     if(inputName.value!="" && inputDesc.value!="")
     {
         console.log(project.projectList)
-        sideHeader.insertAdjacentHTML("beforeend",`<div class='project-list' id='${inputName.value}'>${inputName.value}</div>`)
+        projectDisplay.insertAdjacentHTML("beforeend",`<div class='project-list' id='${inputName.value}'>${inputName.value}<button class="remove-project" id='${inputName.value}'>Remove Project</button></div>`)
     }
 
     // document.getElementById("myform").reset(); 
@@ -34,18 +38,36 @@ submitDialogBtn.addEventListener('click',(e)=>{
 
 
 })
+// Add task logic
 submitTaskBtn.addEventListener('click',()=>{
     // console.log(currentProject.tasks)
     currentProject.tasks.push(inputTask.value); 
     // console.log(currentProject.tasks)
     let taskListToDisplay=""; 
-    for(let i of currentProject.tasks){
-        taskListToDisplay+=`<div>${i}</div>`
+    for(let i in currentProject.tasks){
+        taskListToDisplay+=`<div>${currentProject.tasks[i]}<span><button id='${i}' class="delete-task">Delete</button></span></div>`
     }
     console.log(taskListToDisplay)
     taskContent.innerHTML=`<div id="${currentProject.name}">${currentProject.name} <div>${currentProject.description}</div><button id="AddTask">Add Task</button></div>${taskListToDisplay}`;
 })
 
+//RemoveTask logic
+taskContent.addEventListener("click",(e)=>{
+    console.log("delete task button clicked",e.target); 
+    if(e.target && e.target.matches(".delete-task")){
+        let taskIndex=e.target.getAttribute("id"); 
+        console.log(taskIndex); 
+        console.log("delete task clicked"); 
+        currentProject.tasks.splice(taskIndex,1); 
+        let taskListToDisplay=""; 
+    for(let i in currentProject.tasks){
+        taskListToDisplay+=`<div>${currentProject.tasks[i]}<span><button id='${i}' class="delete-task">Delete</button></span></div>`
+    }
+    // console.log(taskListToDisplay)
+    taskContent.innerHTML=`<div id="${currentProject.name}">${currentProject.name} <div>${currentProject.description}</div><button id="AddTask">Add Task</button></div>${taskListToDisplay}`;
+    }
+})
+// Project load logic 
 sideHeader.addEventListener("click",(e)=>{
     if(e.target && e.target.className=="project-list"){
         console.log("button clicked"); 
@@ -58,12 +80,33 @@ sideHeader.addEventListener("click",(e)=>{
         }
         currentProject=projectContent; 
         let taskListToDisplay=""; 
-        console.log(projectContent.tasks,currentProject.tasks); 
-        for(let i of currentProject.tasks){
-            taskListToDisplay+=`<div>${i}</div>`
+        console.log(currentProject.tasks); 
+        for(let i in currentProject.tasks){
+            console.log(i)
+            taskListToDisplay+=`<div>${(currentProject.tasks)[i]}<span><button id='${i}' class="delete-task">Delete</button></span></div>`
         }
         console.log(taskListToDisplay)
-        taskContent.innerHTML=`<div id="${projectContent.name}">${projectContent.name} <div>${projectContent.description}</div><button id="AddTask">Add Task</button></div>${taskListToDisplay}`;
+        taskContent.innerHTML=`<div id="${projectContent.name}">${projectContent.name} <div>${projectContent.description}</div><button id="AddTask">Add Task</button>${taskListToDisplay}`;
     }
 
+})
+
+//Remove Project 
+sideHeader.addEventListener("click",(e)=>{
+    if(e.target && e.target.matches(".remove-project")){
+        console.log("delete project clicked"); 
+        for(let i in project.projectList){
+            if(project.projectList[i].name==e.target.id){
+                project.projectList.splice(i,1)
+            }
+        }
+        console.log(project.projectList)
+        projectDisplay.innerHTML=""
+        // sideHeader.insertAdjacentHTML("beforeend",`<div class='project-list' id='${inputName.value}'>${inputName.value}<button class="remove-project" id='${inputName.value}'>Remove Project</button></div>`)
+        for(let j of project.projectList){
+            projectDisplay.insertAdjacentHTML("beforeend",`<div class='project-list' id='${j.name}'>${j.name}<button class="remove-project" id='${j.name}'>Remove Project</button></div>`)
+        }
+        taskContent.innerHTML=""; 
+        currentProject=null; 
+    }
 })
